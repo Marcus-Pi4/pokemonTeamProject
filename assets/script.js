06387b30bbb0d8dc4f91cc086fdb78431da734ec
Kalyn Reid <reidkalyn361@gmail.com>
2021-06-17 20:02:00 -0700

    Added collaspable table funtions and changed search button.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const filterSwitch = document.getElementById("filter-switch").parentNode;
 const filterTable = document.getElementById("filter-table");
 const filterList = document.getElementById("filter-list");
+const tableButton = document.getElementById("expand-table")
 console.log(filterSwitch);
 console.log(filterTable);
 
@@ -13,5 +14,16 @@ function hideTable() {
     }
 }
 
+function hideList() {
+    console.log(filterList.getAttribute("style"));
+    let styleAtt = filterList.getAttribute("style");
+    if (styleAtt==="display: none;"){
+        filterList.setAttribute("style", "display: content;");
+    } else{
+        filterList.setAttribute("style", "display: none;");
+    }
+}
+
 
 filterSwitch.addEventListener("click", hideTable);
+tableButton.addEventListener("click", hideList);
